166d08098293c63862c2cd34d0e3632c581b3726
abdullah.shaikh.1@gmail.com <abdullah.shaikh.1@gmail.com@b19850fa-dfe1-11de-ab17-5d3be3536607>
2011-02-05 14:36:47 +0000

    Checkin new version

--- trunk/ofbizify/trunk/src/com/ofbizify/gwt/gwtrpc/serializer/PayloadSerializer.java
+++
@@ -1,9 +0,0 @@
-package com.ofbizify.gwt.gwtrpc.serializer;
-
-import com.google.gwt.user.client.rpc.SerializationException;
-
-public interface PayloadSerializer<E> {
-
-	public Object serialize(E instance) throws SerializationException;
-	public Object deserialize(E instance) throws SerializationException;
-}
